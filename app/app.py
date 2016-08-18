f4f6d762095d1c09ec654e81cfa4932c735634b2
hyteer <hyteer@qq.com>
2016-08-19 00:33:01 +0800

    Form&DB

--- app/app.py
+++ app/app.py
@@ -1,26 +1,79 @@
 # encoding:utf-8
 '''
-This script could be run with commands
+    Flask Demo App
+    By: YT
+    2016.8
 '''
-
-from flask import Flask, redirect, abort, render_template
+import os
+from datetime import datetime
+from flask import Flask, redirect,session, abort, render_template,url_for,flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+
 from model import Users
 from model.users import load_user
 
+#### App initializing ####
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-#### Def Routes
+#### Config ####
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SECRET_KEY'] = 'my secret string'
+
+db = SQLAlchemy(app)
+
+
+#### Form Class ####
+class NameForm(Form):
+    name = StringField('What is your anme?',validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+#### Model ####
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64),unique=True)
+    users = db.relationship('User',backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64),unique=True,index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+#### Def Routes ####
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        form.name.data = ''
+        print "%s Requested."%session.get('name')
+        return redirect(url_for('index'))
+
+    return render_template('index.html',form=form,name=session.get('name'),current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def userboot(name):
