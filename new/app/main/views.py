e8c8ab39acfd13bfd54b701f381be05b8188cacf
hyteer <hyteer@qq.com>
2016-08-22 16:31:47 +0800

    errors

--- new/app/main/views.py
+++ new/app/main/views.py
@@ -6,20 +6,8 @@ from . import main
 def index():
     return render_template('index.html')
 
-@main.route('/secret')
-def secret():
-    import pdb; pdb.set_trace()
-    return render_template('user/sec/secret.html')
 
 @main.route('/about')
 def about():
     return render_template('about.html')
 
-#### Special Routes
-@main.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@main.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
--- new/app/user/views.py
+++ new/app/user/views.py
@@ -1,13 +1,16 @@
 from flask import render_template,request
+from flask_login import login_required
 from . import user
 
 
 @user.route('/info')
+@login_required
 def info():
     #import pdb; pdb.set_trace()
     return render_template('user/info.html')
 
 @user.route('/sec')
+@login_required
 def secret():
     #import pdb; pdb.set_trace()
     return render_template('user/sec/secret.html')
