580ddb35117e24f9c38cf075300d40149f0055ff
hyteer <hyteer@qq.com>
2016-08-24 00:22:03 +0800

    roles1

---
+++ bak_copy/8g_org_beforeRoles/app/user/views.py
@@ -0,0 +1,20 @@
+from flask import render_template,request
+from flask_login import login_required
+from . import user
+
+
+@user.route('/info')
+@login_required
+def info():
+    #import pdb; pdb.set_trace()
+    return render_template('user/info.html')
+
+@user.route('/sec')
+@login_required
+def secret():
+    #import pdb; pdb.set_trace()
+    return render_template('user/sec/secret.html')
+
+@user.route('/about')
+def about():
+    return render_template('about.html')
