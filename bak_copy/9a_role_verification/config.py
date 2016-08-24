6f6b61e2d13fe5444696e2326c43b23284d909d5
hyteer <hyteer@qq.com>
2016-08-25 00:34:32 +0800

    role verification

--- bak_copy/9a_role_verification/config.py
+++ bak_copy/9a_role_verification/config.py
@@ -4,15 +4,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SECRET_KEY2 = 'the secret key of yt'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.qq.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_MAIL_SENDER = 'Flasky Admin <hyteer@qq.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'yotong@qq.com'
 
     @staticmethod
     def init_app(app):
